7c06e3fac185f4ca8952105b7a79af0da5ec155e
vipul prajapati <prajapativipul0408@gmail.com>
2025-06-08 21:03:41 -0400

    added-API-NavBtn-Logic

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,42 @@
 import RestoCard from "./RestaurantCard";
-import resObj from "../../utils/mockData";
-import { useState } from "react";
-// import { useState, useEffect } from "react";
+// import resObj from "../../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 // import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-// local state variable by react - insode the body fucntion scope
-const [listOfRestaurants, setListOfRestaurants] = useState(resObj.restaurants);
+  // local state variable by react - insode the body fucntion scope
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
   // let listOfRestaurants = resObj.restaurants;
   const filterRestaurant = () => {
     const filteredRestaurants = listOfRestaurants.filter((res) => res.info.avgRating > 4.2);
-
     setListOfRestaurants(filteredRestaurants);
     console.log(listOfRestaurants);
   }
 
-  return (
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=12.9628669&lng=77.57750899999999&carousel=true&third_party_vendor=1")
+    const json = await data.json();
+
+    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    console.log(json);
+    // console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+  }
+
+  // Conditional Rendering
+  // if(listOfRestaurants.length === 0){
+  //   return <Shimmer></Shimmer>
+  // }
+
+  // Ternary Operator
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer></Shimmer>) : (
     <>
       <div className="filter">
         <button className="filter-btn" onClick={() => { filterRestaurant() }}>Filter Restaurants</button>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,30 @@
+import { useState } from "react";
 import { LOGO_URL } from "../../utils/constants";
 
 export const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_URL}
-            alt=""
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
 
-  export default Header;
+  // let btnName = "Login";
+  const [btnName, setBtnName] = useState("Login")
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={LOGO_URL}
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button className="login" onClick={() => { btnName === "Login" ? setBtnName("Logout") : setBtnName("Login") }}>{btnName}</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

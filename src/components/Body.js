e00fbedc5ed41f38a0738834b18874e728704f3a
vipul prajapati <prajapativipul0408@gmail.com>
2025-06-10 20:53:15 -0400

    added -f search

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,6 +9,10 @@ const Body = () => {
   // local state variable by react - insode the body fucntion scope
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
+  // making a copy of the state variable
+  const [filteredRestaurant,setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("")
+
   // let listOfRestaurants = resObj.restaurants;
   const filterRestaurant = () => {
     const filteredRestaurants = listOfRestaurants.filter((res) => res.info.avgRating > 4.2);
@@ -24,7 +28,8 @@ const Body = () => {
     const data = await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=12.9628669&lng=77.57750899999999&carousel=true&third_party_vendor=1")
     const json = await data.json();
 
-    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     console.log(json);
     // console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
   }
@@ -32,19 +37,34 @@ const Body = () => {
   // Conditional Rendering
   // if(listOfRestaurants.length === 0){
   //   return <Shimmer></Shimmer>
-  // }
+  // } OR below approach
 
   // Ternary Operator
   return listOfRestaurants.length === 0 ? (
     <Shimmer></Shimmer>) : (
     <>
       <div className="filter">
+        <div className="search">
+          <input type="text" value={searchText} placeholder="Enter restaurant name" onChange={(e) => setSearchText(e.target.value)} />
+          <button onClick={() => {
+            const filteredRestaurant = listOfRestaurants.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            console.log(filteredRestaurant);
+
+            setFilteredRestaurant(filteredRestaurant);
+
+          }
+
+          }>Search</button>
+        </div>
         <button className="filter-btn" onClick={() => { filterRestaurant() }}>Filter Restaurants</button>
       </div>
       <div className="swiggy-restaurants">
         <div className="card">
 
-          {listOfRestaurants.map((restaurant) => <RestoCard key={restaurant.info.id} resData={restaurant} />)}
+          {filteredRestaurant.map((restaurant) => <RestoCard key={restaurant.info.id} resData={restaurant} />)}
+          
         </div>
       </div>
     </>
